ab6312f72faf2026ebc69e3a6afc4100e4e13886
Oğuzhan Çelik <163570717+Oguzhan-Celikk@users.noreply.github.com>
2026-02-15 21:47:59 +0300

    Update gesture_engine.py

--- gesture_engine.py
+++ gesture_engine.py
@@ -9,7 +9,7 @@ mp_draw = mp.solutions.drawing_utils
 cap = cv2.VideoCapture(0) # Bilgisayarının kamerasını açar
 
 while cap.isOpened():
-    success, img = cap.read()
+    success, img = cap.read() 
     if not success: break
 
     # Görüntüyü yatayda çevir (Ayna etkisi)
