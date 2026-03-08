475745ccbb0d153917634132009cd71ba52ae623
Oğuzhan Çelik <163570717+Oguzhan-Celikk@users.noreply.github.com>
2026-03-08 21:35:15 +0300

    Update gestures.py

--- gestures.py
+++ gestures.py
@@ -5,7 +5,7 @@ import requests
 
 class GestureEngine:
     def __init__(self):
-        self.mp_hands = mp.solutions.hands
+        self.mp_hands = mp.solutions.hands 
         self.hands = self.mp_hands.Hands(
             max_num_hands=1, 
             min_detection_confidence=0.7,
